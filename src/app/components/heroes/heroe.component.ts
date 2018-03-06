5496685ffd33c0e3c483eb13a411bdf918d40bc0
Diego Fabricio <difagume@gmail.com>
2018-03-05 21:49:55 -0500

    HTTP - GET - Obtener un nodo específico

--- src/app/components/heroes/heroe.component.ts
+++ src/app/components/heroes/heroe.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Heroe } from '../../interfaces/heroe.interface';
 import { HeroesService } from '../../services/heroes.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
@@ -17,19 +17,61 @@ export class HeroeComponent implements OnInit {
     casa: 'Marvel'
   };
 
+  nuevo = false;
+  id: string;
+
   constructor(private _heroesService: HeroesService,
-    private router: Router) { }
+    private router: Router,
+    private activatedRoute: ActivatedRoute) {
+
+    this.activatedRoute.params
+      .subscribe(parametros => {
+
+        // Obtengo el id del url
+        console.log(parametros);
+        this.id = parametros['id'];
+
+        // Voy a cargar los datos del héroe
+        if (this.id !== 'nuevo') {
+
+          this._heroesService.getHeroe(this.id)
+            .subscribe(data => this.heroe = data);
+        }
+
+      });
+
+  }
 
   ngOnInit() {
   }
 
   guardar() {
     console.log(this.heroe);
-    this._heroesService.nuevoHeroe(this.heroe)
-      .subscribe(data => {
-        this.router.navigate(['/heroe', data.name]);
-      },
-        error => console.error(error));
+
+    if (this.id === 'nuevo') {
+      // insertando
+      this._heroesService.nuevoHeroe(this.heroe)
+        .subscribe(data => {
+          this.router.navigate(['/heroe', data.name]);
+        },
+          error => console.error(error));
+
+    } else {
+      // actualizando
+      this._heroesService.actualizarHeroe(this.heroe, this.id)
+        .subscribe(data => {
+          console.log(data);
+        },
+          error => console.error(error));
+    }
+  }
+
+  agregarNuevo(forma: NgForm) {
+    this.router.navigate(['/heroe', 'nuevo']);
+    // Reset de la forma
+    forma.reset({
+      casa: 'Marvel'
+    });
   }
 
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -7,12 +7,13 @@ import 'rxjs/Rx';
 export class HeroesService {
 
   firebaseUrl = 'https://heroesapp-3e5f5.firebaseio.com/heroes.json';
+  heroeUrl = 'https://heroesapp-3e5f5.firebaseio.com/heroes/';
 
   constructor(private http: Http) { }
 
   nuevoHeroe(heroe: Heroe) {
-    let body = JSON.stringify(heroe);
-    let headers = new Headers({
+    const body = JSON.stringify(heroe);
+    const headers = new Headers({
       'Content-Type': 'application/json'
     });
 
@@ -21,6 +22,28 @@ export class HeroesService {
         console.log(res.json());
         return res.json();
       });
+  }
+
+  actualizarHeroe(heroe: Heroe, key$: string) {
+    const body = JSON.stringify(heroe);
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+
+    const url = `${this.heroeUrl}/${key$}.json`;
+
+    return this.http.put(url, body, { headers })
+      .map(res => {
+        console.log(res.json());
+        return res.json();
+      });
+  }
+
+  getHeroe(key$: string) {
+    const url = `${this.heroeUrl}/${key$}.json`;
+
+    return this.http.get(url)
+      .map(res => res.json());
   }
 
 }
