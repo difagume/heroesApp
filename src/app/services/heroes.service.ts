dfa23b36bc7e20b06547dbf00bc8ff18469f9997
Diego Fabricio <difagume@gmail.com>
2018-03-05 20:39:14 -0500

    HTTP - POST - Creando un nuevo registro en Firebase

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Heroe } from '../interfaces/heroe.interface';
+import 'rxjs/Rx';
 
 @Injectable()
 export class HeroesService {
 
-  constructor() { }
+  firebaseUrl = 'https://heroesapp-3e5f5.firebaseio.com/heroes.json';
+
+  constructor(private http: Http) { }
+
+  nuevoHeroe(heroe: Heroe) {
+    let body = JSON.stringify(heroe);
+    let headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.post(this.firebaseUrl, body, { headers })
+      .map(res => {
+        console.log(res.json());
+        return res.json();
+      });
+  }
 
 }
